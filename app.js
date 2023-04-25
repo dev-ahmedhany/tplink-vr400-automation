d18e3523de76ae9c658c2e0197fe3141750261a6
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2023-04-26 00:11:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,33 +12,70 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     button1: "#menuTree > li:nth-child(12) > a",
     button2: "#menuTree > li:nth-child(12) > ul > li:nth-child(7) > a",
   }
+  const timeToDelay = 1000;
 
   const browser = await puppeteer.launch({headless: "new"});
   const page = await browser.newPage();
+  await page.setViewport({
+    width: 1024,
+    height: 1024,
+    deviceScaleFactor: 1,
+  });
   await page.goto(process.env.URL);
 
 
-  const buttonClick = async (selector) => {
-    await page.waitForSelector(selector);
-    await delay(500);
-    await page.evaluate((btnSelector) => {document.querySelector(btnSelector).click();}, selector);
+  const buttonClick = async (selector,previous) => {
+    try {
+      await page.waitForSelector(selector);
+      await delay(timeToDelay);
+      await page.click(selector);
+    } catch (error) {
+      try {
+        await previous()
+        await page.waitForSelector(selector);
+        await delay(timeToDelay);
+        await page.click(selector);
+      } catch (error) {
+        console.error("error",error);
+      }
+    }
   }
 
   try {
     await page.waitForSelector(selectors.passwordField);
-    await page.type(selectors.passwordField, process.env.PASSWORD);
+    await delay(timeToDelay);
+    await page.type(selectors.passwordField, process.env.PASSWORD,{delay: 100});
     await buttonClick(selectors.loginButton);
     try {
       await buttonClick(selectors.confirmButton);
     } catch (error) {
       console.log("no confirm", error);
     }
-    await buttonClick(selectors.advancedButton);
-    await buttonClick(selectors.button1);
-    await buttonClick(selectors.button2);
+    await buttonClick(selectors.advancedButton , async () => {
+      await page.waitForSelector(selectors.passwordField);
+        await delay(timeToDelay);
+        await page.type(selectors.passwordField, process.env.PASSWORD,{delay: 100});
+        await buttonClick(selectors.loginButton);
+        try {
+          await buttonClick(selectors.confirmButton);
+        } catch (error) {
+          console.log("no confirm", error);
+        }
+      }
+      );
+    await buttonClick(selectors.button1, async () => {
+      await buttonClick(selectors.advancedButton, () => {})})
+
+    await buttonClick(selectors.button2, async () => {
+      await buttonClick(selectors.button1, () => {})})
   
   
-    await page.waitForSelector("#traffic-stat > tbody > tr:nth-child(1)");
+      try {
+        await page.waitForSelector("#traffic-stat > tbody > tr:nth-child(1)");
+      } catch (error) {
+        await buttonClick(selectors.button2, async () => {
+          await buttonClick(selectors.button1, () => {})})
+      }
   
     const result = await page.evaluate(() => {
       const rows = document.querySelectorAll('#traffic-stat tr');
