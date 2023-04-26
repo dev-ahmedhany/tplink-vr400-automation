3a721bc6707380f4177eadaa79ee818a41ba4ad4
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-26 03:49:00 +0200

    puppeteer

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 // puppeteer-extra is a drop-in replacement for puppeteer,
 // it augments the installed puppeteer with plugin functionality
-const puppeteer = require('puppeteer-extra')
-
-// add stealth plugin and use defaults (all evasion techniques)
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
+const puppeteer = require('puppeteer')
 
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
