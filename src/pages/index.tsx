0fd3bbadc442e5b9028b3cd134ed8d281d1bf0b4
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2025-08-17 16:58:34 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import { Inter } from "next/font/google";
 import { useState, useEffect } from "react";
 import styles from "@/styles/Home.module.css";
